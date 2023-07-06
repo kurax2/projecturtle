f9fa65734ddadfbe9e24f2c147d509c735202447
kurax2 <107274232+kurax2@users.noreply.github.com>
2023-07-06 23:42:45 +0800

    edit edit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import { CssBaseline, Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          project turtles in progress ...
-        </p>
         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <InstagramIcon/>
-        </a>
+        <p>
+        projecturtle in progress ...
+        </p>
+         
+       
+        
+        
       </header>
+
+      <Bio/>
+      <Socials/>
+      
     </div>
   );
+}
+
+function Bio(){
+  return(
+    <>
+      <CssBaseline/>
+      <Typography variant='body1'>hi, Kura here </Typography>
+      <Typography variant='body1'>i'm currently learning </Typography>
+      <img src={logo} className="App-logo" alt="logo"/>
+    </>
+  )
+}
+
+function Socials(){
+  return(
+    <>
+      <CssBaseline/>
+        <div>
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InstagramIcon/>
+        </a>
+        </div>
+        
+    </>
+  
+  )
 }
 
 export default App;
